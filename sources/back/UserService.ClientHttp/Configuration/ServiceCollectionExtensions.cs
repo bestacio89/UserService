1d9d24f833443afc3df426e2779b592c34d3f9d8
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:52 +0200

    [Feat][Client.Http]: Register HTTP clients for dependency injection

---
+++ sources/back/UserService.ClientHttp/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserService.Client.Http.Clients;
+
+namespace UserService.Client.Http.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+  public static IServiceCollection AddUserServiceHttpClient(
+      this IServiceCollection services,
+      string baseUrl)
+  {
+    services.AddHttpClient<UsersClient>(c => c.BaseAddress = new Uri(baseUrl));
+    services.AddHttpClient<AdminUsersClient>(c => c.BaseAddress = new Uri(baseUrl));
+    services.AddHttpClient<RankedClient>(c => c.BaseAddress = new Uri(baseUrl));
+    services.AddHttpClient<ProgressionClient>(c => c.BaseAddress = new Uri(baseUrl));
+    services.AddHttpClient<IdentityClient>(c => c.BaseAddress = new Uri(baseUrl));
+
+    return services;
+  }
+}
