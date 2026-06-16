f652adbebcb1040aa295410114d9bf8398fdcf83
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:52 +0200

    [Feat][Client.Http]: Add base HTTP client abstraction

---
+++ sources/back/UserService.ClientHttp/Abstractions/HttpClientBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+
+namespace UserService.Client.Http.Abstractions;
+
+public abstract class HttpClientBase
+{
+  protected readonly HttpClient Http;
+
+  protected HttpClientBase(HttpClient http)
+  {
+    Http = http;
+  }
+
+  protected async Task<T> GetAsync<T>(string url, CancellationToken ct)
+  {
+    var result = await Http.GetFromJsonAsync<T>(url, ct);
+    return result!;
+  }
+
+  protected async Task<TResponse> PostAsync<TRequest, TResponse>(
+      string url,
+      TRequest request,
+      CancellationToken ct)
+  {
+    var response = await Http.PostAsJsonAsync(url, request, ct);
+    response.EnsureSuccessStatusCode();
+
+    return (await response.Content.ReadFromJsonAsync<TResponse>(ct))!;
+  }
+
+  protected async Task PostAsync<TRequest>(
+      string url,
+      TRequest request,
+      CancellationToken ct)
+  {
+    var response = await Http.PostAsJsonAsync(url, request, ct);
+    response.EnsureSuccessStatusCode();
+  }
+}
