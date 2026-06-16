ac1fbb2a4c4bb6d73a73b5c3f5f7db6b86ded48f
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:46 +0200

    [Feat][Domain]: Add Identity domain model

---
+++ sources/back/UserService.Domain/Identity/IdentityProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Domain.Identity;
+
+public enum IdentityProvider
+{
+  Guest,
+  Google,
+  Apple
+}
---
+++ sources/back/UserService.Domain/Identity/UserIdentity.cs
@@ -0,0 +1,30 @@
+﻿using Franz.Common.Business.Domain;
+
+namespace UserService.Domain.Identity;
+
+public sealed class UserIdentity : Entity<Guid>
+{
+  public Guid UserId { get; private set; }
+
+  public IdentityProvider Provider { get; private set; }
+
+  public string ExternalId { get; private set; } = null!;
+
+  public string? DeviceId { get; private set; }
+
+  private UserIdentity() { }
+
+  public void Define(
+      Guid userId,
+      IdentityProvider provider,
+      string externalId,
+      string? deviceId)
+  {
+    UserId = userId;
+    Provider = provider;
+    ExternalId = externalId;
+    DeviceId = deviceId;
+    MarkCreated("system");
+  }
+}
+
