483e100fdccc202e1acfe5e47193a67531b15b41
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:46 +0200

    [Feat][Domain]: Introduce User core aggregate and state model

---
+++ sources/back/UserService.Domain/Users/User.cs
@@ -0,0 +1,84 @@
+﻿using Franz.Common.Business.Domain;
+
+namespace UserService.Domain.Users;
+
+public sealed class User : Entity<Guid>
+{
+  public string Username { get; private set; } = null!;
+
+  public UserAccountState State { get; private set; }
+
+  public string? StatusReason { get; private set; }
+
+  public DateTime? SuspendedUntil { get; private set; }
+
+  private User() { }
+
+  public void Define(string username)
+  {
+    Username = username;
+
+    State = UserAccountState.Active;
+    StatusReason = null;
+    SuspendedUntil = null;
+
+    MarkCreated("system");
+  }
+
+  public void Ban(string reason)
+  {
+    if (string.IsNullOrWhiteSpace(reason))
+    {
+      throw new ArgumentException(
+          "A ban reason is required.",
+          nameof(reason));
+    }
+
+    State = UserAccountState.Banned;
+    StatusReason = reason;
+    SuspendedUntil = null;
+
+    MarkUpdated("system");
+  }
+
+  public void Suspend(
+      string reason,
+      DateTime? until)
+  {
+    if (string.IsNullOrWhiteSpace(reason))
+    {
+      throw new ArgumentException(
+          "A suspension reason is required.",
+          nameof(reason));
+    }
+
+    State = UserAccountState.Suspended;
+    StatusReason = reason;
+    SuspendedUntil = until;
+
+    MarkUpdated("system");
+  }
+
+  public void Reactivate()
+  {
+    State = UserAccountState.Active;
+    StatusReason = null;
+    SuspendedUntil = null;
+
+    MarkUpdated("system");
+  }
+
+  public void ChangeUsername(string username)
+  {
+    if (string.IsNullOrWhiteSpace(username))
+    {
+      throw new ArgumentException(
+          "Username cannot be empty.",
+          nameof(username));
+    }
+
+    Username = username;
+
+    MarkUpdated("system");
+  }
+}
---
+++ sources/back/UserService.Domain/Users/UserAccountState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Domain.Users;
+
+public enum UserAccountState
+{
+  Active,
+  Suspended,
+  Banned
+}
