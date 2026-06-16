15c215c65ead06828642fffa1eada667f57b9525
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:47 +0200

    [Feat][Domain]: Add Progression domain models

---
+++ sources/back/UserService.Domain/Progression/UserClassMastery.cs
@@ -0,0 +1,29 @@
+﻿using Franz.Common.Business.Domain;
+
+namespace UserService.Domain.Progression;
+
+public sealed class UserClassMastery : Entity<Guid>
+{
+  public Guid UserId { get; private set; }
+
+  public Guid HeroClassId { get; private set; }
+
+  public int MasteryLevel { get; private set; }
+
+  public int Experience { get; private set; }
+
+  public void AddExperience(int xp)
+  {
+    Experience += xp;
+
+    // simple deterministic progression rule (can evolve later)
+    if (Experience >= RequiredXpForNextLevel())
+    {
+      MasteryLevel++;
+      Experience = 0;
+    }
+  }
+
+  private int RequiredXpForNextLevel()
+      => 100 + (MasteryLevel * 25);
+}
---
+++ sources/back/UserService.Domain/Progression/UserHeroMastery.cs
@@ -0,0 +1,31 @@
+﻿using Franz.Common.Business.Domain;
+
+namespace UserService.Domain.Progression;
+
+public sealed class UserHeroMastery : Entity<Guid>
+{
+  public Guid UserId { get; private set; }
+
+  public Guid HeroId { get; private set; }
+
+  public int MasteryLevel { get; private set; }
+
+  public int Experience { get; private set; }
+
+  public int GamesPlayed { get; private set; }
+
+  public void RegisterGame(bool win, int xpGain)
+  {
+    GamesPlayed++;
+    Experience += xpGain;
+
+    if (Experience >= RequiredXpForNextLevel())
+    {
+      MasteryLevel++;
+      Experience = 0;
+    }
+  }
+
+  private int RequiredXpForNextLevel()
+      => 120 + (MasteryLevel * 30);
+}
