4b1eeb439bbf5fd15373800ea84c7816f8aaa4f0
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-16 13:27:47 +0200

    [Feat][Domain]: Add Ranked domain model

---
+++ sources/back/UserService.Domain/Ranked/UserRank.cs
@@ -0,0 +1,30 @@
+﻿using Franz.Common.Business.Domain;
+
+namespace UserService.Domain.Ranked;
+
+public sealed class UserRank : Entity<Guid>
+{
+  public Guid UserId { get; private set; }
+
+  public int MMR { get; private set; }
+
+  public int Wins { get; private set; }
+
+  public int Losses { get; private set; }
+
+  public void ApplyMatchResult(bool win, int mmrDelta)
+  {
+    if (win)
+    {
+      Wins++;
+      MMR += mmrDelta;
+    }
+    else
+    {
+      Losses++;
+      MMR -= mmrDelta;
+    }
+
+    if (MMR < 0) MMR = 0;
+  }
+}
