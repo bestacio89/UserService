16628436aa7f327b26c999d3cc74cdddec860cc8
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2026-06-21 16:10:26 +0200

    [Feat][Application]: introduce authentication context and step interface

---
+++ sources/back/UserService.Application/Commands/Authentication/AuthenticationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserService.Contracts.Commands.Authentication;
+using UserService.Contracts.DTOs.Authentication;
+using UserService.Domain.Authentication;
+using UserService.Domain.Identity;
+using UserService.Domain.Users;
+
+namespace UserService.Application.Commands.Authentication;
+
+public sealed class AuthenticationContext
+{
+  public LoginUserCommand Command { get; init; }
+
+  public ExternalIdentity ExternalIdentity { get; set; }
+  public UserIdentity UserIdentity { get; set; }
+  public User User { get; set; }
+  public UserSession Session { get; set; }
+
+  public string AccessToken { get; set; }
+  public string RefreshToken { get; set; }
+}
---
+++ sources/back/UserService.Application/Commands/Authentication/Services/IAuthenticationStep.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+namespace UserService.Application.Commands.Authentication.Services;
+
+public interface IAuthenticationStep
+{
+  Task ExecuteAsync(AuthenticationContext context, CancellationToken ct);
+}
